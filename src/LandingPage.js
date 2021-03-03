0f4319eb6b214a485883a9fa9a9977538e310d6a
Megan <calvem2@cs.washington.edu>
2021-03-03 12:48:29 -0800

    add join button.

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,23 +1,40 @@
 import { Button } from 'react-bootstrap';
 import './LandingPage.css';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import background from "./images/landing-bg.png";
+import React, { useState } from 'react';
 
 function LandingPage() {
+    let history = useHistory();
+    const [code, setCode] = useState(0);
+
+    function onJoinSubmit(event) {
+        history.push({
+            pathname: '/toe',
+            state: {code: code}
+        });
+    }
+
     return(
         <main className="landing-container">
             <img className="background" src={background} />
                 <div>
                     <h1 className="top-header">guess who?</h1>
                     <div className="launch-container">
-                        <form>
+                        <form  onSubmit={onJoinSubmit}>
                             <label>
-                                <input type="text" name="code" placeholder="ENTER ROOM CODE"></input>
+                                <input
+                                    type="text"
+                                    name="code"
+                                    onChange={(e) => setCode(e.target.value)}
+                                    placeholder="ENTER ROOM CODE">
+                                </input>
                             </label>
+                            <Button type="submit" className="btn btn-join">JOIN GAME</Button>
                         </form>
                         <p>or</p>
                         <Link to="/prep">
-                            <button className="btn btn-info">START YOUR OWN GAME</button>
+                            <Button className="btn btn-info btn-start">START YOUR OWN GAME</Button>
                         </Link>
                     </div>
                 </div>
