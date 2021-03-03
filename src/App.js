bd360508f43cfef48733beea6f0a5116fcac1d7f
Megan <calvem2@cs.washington.edu>
2021-03-03 09:47:53 -0800

    move nav bar to bottom on landing page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route, Link, useLocation } from "react-router-dom";
 import LandingPage from './LandingPage.js';
 import About from './About.js';
 import Privacy from './Privacy.js';
@@ -12,10 +14,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
+  // const { location: { pathname } } = props;
+  // let params = useLocation();
+  // console.log(params);
+
   return (
     <BrowserRouter>
       <div className="app-container">
-      <nav className="nav-bar">
+        <NavBar/>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/privacy" component={Privacy} />
+          <Route path="/how" component={HowToPlay}/>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/toe" component={Toe} />
+          <Route path="/prep" component={SetUp} />
+
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+const NavBar  = () => {
+  const location = useLocation();
+  const navClass = location.pathname === "/"
+      ? "nav-bar-bottom"
+      : "nav-bar";
+  return (
+      <nav className={navClass}>
         <ul className="nav-container">
           <li className="home">
             <Link exact to="/" className="nav-link">guess who?</Link>
@@ -35,26 +65,9 @@ function App() {
           <li>
             <Link to="/prep" className="nav-link">Prep</Link>
           </li>
-          
-
         </ul>
       </nav>
-
-      {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-      <Switch>
-        <Route path="/about" component={About} />
-        <Route path="/privacy" component={Privacy} />
-        <Route path="/how" component={HowToPlay}/>
-        // todo: change back to landing page
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/toe" component={Toe} />
-        <Route path="/prep" component={SetUp} />
-
-      </Switch>
-    </div>
-    </BrowserRouter>
-  );
-}
+  )
+};
 
 export default App;
